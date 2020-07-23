b7edf412d8d7f1a09a29d1b9692ff8038a8db59f
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-07-23 19:51:40 +0300

    less spam

--- src/eratuonti.js
+++ src/eratuonti.js
@@ -19,9 +19,9 @@ export default function ({apiUrl, apiUsername, apiPassword, apiClientUserAgent, 
     const blob = JSON.stringify(linkedValids);
     const type = 'application/json';
     const profile = linkDataHarvesterApiProfileId;
-    logger.log('debug', profile);
-    logger.log('debug', type);
-    logger.log('debug', blob);
+    // Logger.log('debug', profile);
+    // Logger.log('debug', type);
+    // Logger.log('debug', blob);
     logger.log('info', 'Data sending to Erätuonti service has begun!');
     if (blob) { // eslint-disable-line functional/no-conditional-statement
       try {
