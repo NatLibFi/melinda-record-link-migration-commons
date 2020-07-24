abd3ab43c1c0f4bfa83ab8a449a95870859d2a8b
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-07-24 07:32:48 +0300

    fix pushBlobIds

--- src/mongo.js
+++ src/mongo.js
@@ -46,6 +46,7 @@ export default async function (mongoUrl) {
       jobId,
       jobState,
       jobConfig,
+      blobIds: [],
       creationTime: moment().toDate(),
       modificationTime: moment().toDate()
     };
