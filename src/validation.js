4f8904799d4d5655990502069cca1c47f1e87e37
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-07-31 08:55:19 +0300

    parse record validators from string

--- src/validation.js
+++ src/validation.js
@@ -5,8 +5,6 @@ import {
   FieldExclusion as fieldExclusion,
   FieldsPresent as fieldsPresent,
   FieldStructure as fieldStructure,
-  FixedFields as fixedFields,
-  ResolvableExtReferences as resolvableExtReferences,
   SubfieldExclusion as subfieldExclusion
 } from '@natlibfi/marc-record-validators-melinda';
 import {Utils} from '@natlibfi/melinda-commons';
@@ -19,36 +17,16 @@ export async function createValidationFactory(validationFactoryOptions) {
   logger.log('silly', `with options: ${JSON.stringify(validationFactoryOptions)}`);
   const factoryOptions = [];
   if (validationFactoryOptions.fieldExclusion !== undefined) { // eslint-disable-line functional/no-conditional-statement
-    factoryOptions.push(await fieldExclusion(fieldExclusionStringsToRegExps(validationFactoryOptions.fieldExclusion))); // eslint-disable-line functional/immutable-data
+    factoryOptions.push(await fieldExclusion(parseValidatorPump(validationFactoryOptions.fieldExclusion))); // eslint-disable-line functional/immutable-data
   }
   if (validationFactoryOptions.fieldsPresent !== undefined) { // eslint-disable-line functional/no-conditional-statement
-    const regExps = validationFactoryOptions.fieldsPresent.map(config => new RegExp(`${config}`, 'u'));
-    factoryOptions.push(await fieldsPresent(regExps)); // eslint-disable-line functional/immutable-data
+    factoryOptions.push(await fieldsPresent(parseValidatorPump(validationFactoryOptions.fieldsPresent))); // eslint-disable-line functional/immutable-data
   }
   if (validationFactoryOptions.fieldStructure !== undefined) { // eslint-disable-line functional/no-conditional-statement
-    const regExps = validationFactoryOptions.fieldStructure.map(config => {
-      const subfields = Object.create(null);
-      Object.entries(config.subfields).forEach(([key, value]) => {
-        subfields[key] = {pattern: new RegExp(`${value}`, 'u')}; // eslint-disable-line functional/immutable-data
-      });
-      return {tag: new RegExp(`${config.tag}`, 'u'), subfields};
-    });
-    factoryOptions.push(await fieldStructure(regExps)); // eslint-disable-line functional/immutable-data
-  }
-  if (validationFactoryOptions.fixedFields !== undefined) { // eslint-disable-line functional/no-conditional-statement
-    const regExps = validationFactoryOptions.fixedFields.map(config => new RegExp(`${config}`, 'u'));
-    factoryOptions.push(await fixedFields(regExps)); // eslint-disable-line functional/immutable-data
-  }
-  if (validationFactoryOptions.resolvableExtReferences !== undefined) { // eslint-disable-line functional/no-conditional-statement
-    const regExps = validationFactoryOptions.resolvableExtReferences.map(config => new RegExp(`${config}`, 'u'));
-    factoryOptions.push(await resolvableExtReferences(regExps)); // eslint-disable-line functional/immutable-data
+    factoryOptions.push(await fieldStructure(parseValidatorPump(validationFactoryOptions.fieldStructure))); // eslint-disable-line functional/immutable-data
   }
   if (validationFactoryOptions.subfieldExclusion !== undefined) { // eslint-disable-line functional/no-conditional-statement
-    const regExps = validationFactoryOptions.subfieldExclusion.map(config => {
-      const subfields = config.subfields.map(sub => ({code: new RegExp(`${sub.code}`, 'u')}));
-      return {tag: new RegExp(`${config.tag}`, 'u'), subfields};
-    });
-    factoryOptions.push(await subfieldExclusion(regExps)); // eslint-disable-line functional/immutable-data
+    factoryOptions.push(await subfieldExclusion(parseValidatorPump(validationFactoryOptions.subfieldExclusion))); // eslint-disable-line functional/immutable-data
   }
 
   await Promise.all(factoryOptions);
@@ -56,16 +34,145 @@ export async function createValidationFactory(validationFactoryOptions) {
   return validateFactory(factoryOptions);
 }
 
-function fieldExclusionStringsToRegExps(fieldExclusion) {
-  return fieldExclusion.map(config => {
-    if (typeof config === 'string') {
-      return new RegExp(`${config}`, 'u');
-    }
+const parserList = [regExpLeader, regExpTag, regExpValue, regExpValuePattern, regExpInd1, regExpInd2, regExpSubfields, booleanStrict, parseDependencies];
 
-    const tag = new RegExp(`${config.tag}`, 'u');
-    const ind1 = config.ind1 ? new RegExp(`${config.ind1}`, 'u') : null;
-    const ind2 = config.ind2 ? new RegExp(`${config.ind2}`, 'u') : null;
-    const subfields = config.subfields ? config.subfields.map(sub => ({code: new RegExp(`${sub.code}`, 'u'), value: new RegExp(`${sub.value}`, 'u')})) : null;
-    return {tag, ind1, ind2, subfields};
-  });
+function parseValidatorPump(values, parsed = []) {
+  const [value, ...rest] = values;
+  if (typeof value === 'string') {
+    return parseValidatorPump(rest, [...parsed, new RegExp(`${value}`, 'u')]);
+  }
+  const parsedValidator = parseRecordValidators(value, parserList);
+  return parseValidatorPump(rest, [...parsed, parsedValidator]);
+}
+
+function parseRecordValidators(object, parsers = parserList) {
+  const [parser, ...rest] = parsers;
+  if (parser === undefined) {
+    return object;
+  }
+
+  return parseRecordValidators(parser(object), rest);
+}
+
+function regExpLeader(object) {
+  if (typeof object.leader === 'string') {
+    return {...object, leader: new RegExp(`${object.leader}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpTag(object) {
+  if (typeof object.tag === 'string') {
+    return {...object, tag: new RegExp(`${object.tag}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpValue(object) {
+  if (typeof object.value === 'string') {
+    return {...object, value: new RegExp(`${object.value}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpValuePattern(object) {
+  if (typeof object.valuePattern === 'string') {
+    return {...object, valuePattern: new RegExp(`${object.valuePattern}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpInd1(object) {
+  if (typeof object.ind1 === 'string') {
+    return {...object, ind1: new RegExp(`${object.ind1}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpInd2(object) {
+  if (typeof object.ind2 === 'string') {
+    return {...object, ind2: new RegExp(`${object.ind2}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpSubfields(object) {
+  if (Array.isArray(object.subfields)) {
+    const subfields = object.subfields.map(sub => {
+      const sub2 = regExpCode(sub);
+      const sub3 = regExpValue(sub2);
+      return sub3;
+    });
+
+    return {...object, subfields};
+  }
+
+  if (typeof object.subfields === 'object') {
+    const subfields = Object.create(null);
+    Object.entries(object.subfields).forEach(([key, value]) => {
+      const value2 = regExpPattern(value);
+      const value3 = booleanRequired(value2);
+      const value4 = numberMaxOccurrence(value3);
+      subfields[key] = value4; // eslint-disable-line functional/immutable-data
+    });
+    return {...object, subfields};
+  }
+
+  return object;
+}
+
+function regExpCode(object) {
+  if (typeof object.code === 'string') {
+    return {...object, code: new RegExp(`${object.code}`, 'u')};
+  }
+
+  return object;
+}
+
+function regExpPattern(object) {
+  if (typeof object.pattern === 'string') {
+    return {...object, pattern: new RegExp(`${object.pattern}`, 'u')};
+  }
+
+  return object;
+}
+
+function booleanRequired(object) {
+  if (typeof object.required === 'string') {
+    return {...object, required: Boolean.parseBoolean(object.required)};
+  }
+
+  return object;
+}
+
+function numberMaxOccurrence(object) {
+  if (typeof object.maxOccurrence === 'string') {
+    return {...object, maxOccurrence: parseInt(object.maxOccurrence, 10)};
+  }
+
+  return object;
+}
+
+function booleanStrict(object) {
+  if (typeof object.strict === 'string') {
+    return {...object, strict: Boolean.parseBoolean(object.stric)};
+  }
+
+  return object;
+}
+
+function parseDependencies(object) {
+  if (Array.isArray(object.dependencies) && object.dependencies.lenght > 0) {
+    const parsers = [regExpLeader, regExpTag, regExpInd1, regExpInd2, regExpValuePattern, regExpSubfields];
+    const parsed = object.dependencies.map(dep => parseRecordValidators(dep, parsers));
+    return {...object, dependencies: parsed};
+  }
+
+  return object;
 }
