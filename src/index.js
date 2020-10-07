62d91da6475d8120c496a978c6037fb706a6dae7
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-10-07 08:17:49 +0300

    export logError

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {createValidationFactory} from './validation';
 import eratuontiFactory from './eratuonti';
 import recordActions from './recordActions';
 import linkDataActions from './linkDataActions';
-export {logError} from './utils';
+import {logError} from './utils';
 
 export {mongoFactory, amqpFactory, createValidationFactory, eratuontiFactory, recordActions, linkDataActions, logError};
 export * from './constants';
