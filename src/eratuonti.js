bb1389b1514da3b9a2b09fd645d9e8c03ae24bf5
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-10-09 09:48:30 +0300

    fix read blob

--- src/eratuonti.js
+++ src/eratuonti.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars, */
 
+import {Error as ApiError} from '@natlibfi/melinda-commons';
 import {createLogger} from '@natlibfi/melinda-backend-commons';
 import {createApiClient} from '@natlibfi/melinda-record-import-commons';
 import {logError} from './utils';
@@ -16,6 +17,9 @@ export default function ({apiUrl, apiUsername, apiPassword, apiClientUserAgent, 
 
   // Send record to transformer
   async function sendBlob(linkedValids) {
+    if (linkedValids === undefined) { // eslint-disable-line functional/no-conditional-statement
+      throw new ApiError(400, 'Valid linked data missing!');
+    }
     const blob = JSON.stringify(linkedValids);
     const type = 'application/json';
     const profile = linkDataHarvesterApiProfileId;
@@ -23,28 +27,25 @@ export default function ({apiUrl, apiUsername, apiPassword, apiClientUserAgent, 
     // logger.log('debug', type);
     // logger.log('debug', blob);
     logger.log('info', 'Data sending to Erätuonti service has begun!');
-    if (blob) { // eslint-disable-line functional/no-conditional-statement
-      try {
-        logger.log('info', 'Trying to create blob');
-        // Record-import-commons: async function createBlob({blob (data), type (constent-type), profile (import profile)})
-        const response = await client.createBlob({blob, type, profile});
-        logger.log('debug', 'Got response');
-        // Return blobId to be saved in jobItem
-        return response;
-        // TRANSFORMER picks it from QUEUE
-      } catch (error) {
-        logError(error);
-        logger.log('error', JSON.stringify(error));
-        return false;
-      }
-    }
 
-    return false;
+    try {
+      logger.log('info', 'Trying to create blob');
+      // Record-import-commons: async function createBlob({blob (data), type (constent-type), profile (import profile)})
+      const response = await client.createBlob({blob, type, profile});
+      logger.log('debug', 'Got response');
+      // Return blobId to be saved in jobItem
+      return response;
+      // TRANSFORMER picks it from QUEUE
+    } catch (error) {
+      logger.log('error', 'Error while sending blob to erätuonti!');
+      logError(error);
+      return false;
+    }
   }
 
   async function readBlob(id) {
     const result = await client.getBlobMetadata({id});
-    logger.log('debug', JSON.stringify(result));
-    return id;
+    // logger.log('debug', JSON.stringify(result));
+    return result;
   }
 }
